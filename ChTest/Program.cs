7d0e24383ef8424c51e10e4a07bd3919a18c0e7e
lehusk <lehu.sk@gmail.com>
2015-10-07 07:44:55 +0200

    init

---
+++ ChTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace ChTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Game();
+        }
+    }
+
+    internal class Player
+    {
+        public GameSide GameSide { get; set; }
+
+        public Player(GameSide gameSide)
+        {
+            GameSide = gameSide;
+        }
+    }
+
+    internal class Move
+    {
+        public FieldLocation From { get; set; }
+        public FieldLocation To { get; set; }
+    }
+
+    internal class FieldLocation
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+    }
+
+    internal enum GameSide
+    {
+        White,
+        Black
+    }
+
+    internal class Board
+    {
+        public List<Field> Fields { get; set; } 
+    }
+}
